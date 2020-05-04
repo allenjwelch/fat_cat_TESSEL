0cafbb8b48476b503cd3cdcac48beb34bcba8967
Allen Welch <allenjwelch@gmail.com>
2020-05-04 09:59:25 -0400

    updates websockets

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const ws = require("nodejs-websocket");
-const socketPORT = 6000;
+const http = require("http");
+const socketIo = require("socket.io");
 require('dotenv').config();
 
+const PORT = process.env.PORT || 4001;
+const routes = require("./routes/index");
+
 const app = express();
-const PORT = process.env.PORT || 9000;
+app.use(routes);
 
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+const server = http.createServer(app);
+const io = socketIo(server);
 
-// app.use(routes);           
+let interval;
 
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
-});
-
-app.listen(PORT, function() {
-    console.log('App listening on PORT ' + PORT);
-});
-
-const server = ws.createServer(function (conn) {
-  console.log("New connection");
-
-  // If we get text from the client, and echo it  
-  conn.on("text", function (str) {
-    // print it out 
-    console.log("Received: "+ str)
-    // Send it back (but more excited)
-    conn.sendText("Message Received!")
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getStatusAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
   });
-
-  // When the client closes the connection, notify us
-  conn.on("close", function (code, reason) {
-      console.log("Connection closed")
-  });
-}).listen(socketPORT, function() {
-    console.log('Socket listening on PORT', socketPORT);
+  socket.on('feed', () => {
+      console.log('feed that bitch');
+  })
 });
 
+const getStatusAndEmit = socket => {
+  const status = 'all good';
+  const foodLevel = 'low';
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("status", status);
+  socket.emit("foodLevel", foodLevel);
+};
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
