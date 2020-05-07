3367e16fc5008cca98c09232ee6f2895081bfe65
awelch <allenjwelch@gmail.com>
2020-05-06 20:42:00 -0400

    updates socket

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require("http");
 const socketIo = require("socket.io");
+const Tessel = require("tessel-io");
+const five = require("johnny-five");
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4001;
@@ -10,31 +13,65 @@ const app = express();
 app.use(routes);
 
 const server = http.createServer(app);
-const io = socketIo(server);
+const socketIO = socketIo(server);
+
+const board = new five.Board({
+  io: new Tessel()
+});
 
 let interval;
 
-io.on("connection", (socket) => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getStatusAndEmit(socket), 1000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-    clearInterval(interval);
+board.on("ready", () => {
+  const led = new five.Led("a5");
+
+  socketIO.on("connection", (socket) => {
+    console.log("New client connected");
+    socket.emit("status", 'Ready');
+
+    if (interval) {
+      clearInterval(interval);
+    }
+    interval = setInterval(() => getFoodLevelAndEmit(socket), 1000);
+    socket.on("disconnect", () => {
+      console.log("Client disconnected");
+      clearInterval(interval);
+    });
+
+    socket.on('feed', (msg) => {
+      console.log('feed that bitch')
+      console.log('portion size: ', msg)
+      socket.emit("status", 'Feeding...');
+
+      
+      let timer;
+
+      if (msg === 'extra') {
+        timer = 5000;
+      } else if (msg === 'snack') {
+        timer = 1000;
+      } else {
+        timer = 3000;
+      }
+
+      led.on();
+
+      setTimeout(() => {
+        led.off()
+        socket.emit("status", 'Ready');
+      }, timer)
+
+    })
   });
-  socket.on('feed', () => {
-      console.log('feed that bitch');
-  })
 });
 
-const getStatusAndEmit = socket => {
-  const status = 'all good';
-  const foodLevel = 'low';
+const getFoodLevelAndEmit = socket => {
+  let foodLevel = 10; 
+  let foodStatusMsg = 'Good';
+  if (foodLevel > 5) {
+    foodStatusMsg = 'Low'
+  }
   // Emitting a new message. Will be consumed by the client
-  socket.emit("status", status);
-  socket.emit("foodLevel", foodLevel);
+  socket.emit("foodLevel", foodStatusMsg);
 };
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
